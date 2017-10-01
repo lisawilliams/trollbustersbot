67670b530c3abf866afe293c34c88da7d756fbd3
lisawilliams <lisa@placeblogger.com>
2017-10-01 10:25:58 -0400

    Adding URLs to list template causes list template to work
    Added urls of web-available images to static_data.js
    which caused list template to load successfully in bot
    
    Ran cleanly without error messages
    
    Responds to Issue#1
    
    Next steps:
    
    * Figure out if I can issue a pull request on templates documentation:
    
    https://botpress.io/examples/fb_send_list
    
    * Make a request to slvn regarding how to activate the "pick" button
    
    Responds to Issue #1

--- static_data.js
+++ static_data.js
@@ -1,11 +1,11 @@
-// from list templates example 
+// from list templates example
 
 module.exports = {
   cities: [
-    { name: 'San Francisco', code: 'SF', image: `${process.env.BOT_BASE_URL}/san_francisco.jpg` },
-    { name: 'New York', code: 'NY', image: `${process.env.BOT_BASE_URL}/new_york.jpg` },
-    { name: 'Frankfurt', code: 'FRA', image: `${process.env.BOT_BASE_URL}/frankfurt.jpg` },
-    { name: 'Budapest', code: 'BUD', image: `${process.env.BOT_BASE_URL}/budapest.jpg` },
-    { name: 'Kuala Lumpur', code: 'KL', image: `${process.env.BOT_BASE_URL}/kuala_lumpur.jpg` }
+    { name: 'San Francisco', code: 'SF', image: `http://waterfire.org/wp-content/uploads/2015/01/waterplace-bridge-Jennifer-Bedford-e1420838154729.jpg` },
+    { name: 'New York', code: 'NY', image: `http://waterfire.org/wp-content/uploads/2015/01/waterplace-bridge-Jennifer-Bedford-e1420838154729.jpg` },
+    { name: 'Frankfurt', code: 'FRA', image: `http://waterfire.org/wp-content/uploads/2015/01/waterplace-bridge-Jennifer-Bedford-e1420838154729.jpg` },
+    { name: 'Budapest', code: 'BUD', image: `http://waterfire.org/wp-content/uploads/2015/01/waterplace-bridge-Jennifer-Bedford-e1420838154729.jpg` },
+    { name: 'Kuala Lumpur', code: 'KL', image: `http://waterfire.org/wp-content/uploads/2015/01/waterplace-bridge-Jennifer-Bedford-e1420838154729.jpg` }
   ]
 }
